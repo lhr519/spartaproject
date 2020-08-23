76421338a0dfa9053f603972b8226b8d9bc022b3
lhr519 <lhr519@naver.com>
2020-08-23 13:32:51 +0900

    200823
    완)식당 일부 이름으로 검색 가능
    카테고리 선택 가능
    완)음식점 아닌 거 제거
    음식점 이름 눌렀을 때 지도에 마커 표시 -> 질문
    완)리뷰 입력 api
    완)마이 리뷰 보여주기 api
    마커 눌렀을 때 길찾기
    마커 좀 더 예쁘게
    사진 저장 ->질문

--- app.py
+++ app.py
@@ -6,29 +6,48 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbtest
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/api/list', methods=['GET'])
+@app.route('/list', methods=['GET'])
 def showList():
-    matjip = list(db.matjip.find({},{'_id':False}))
-    return jsonify({'result': 'success', 'matjip':matjip})
+    matjip = list(db.matjip.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'matjip': matjip})
+
+@app.route('/reviewlist', methods=['GET'])
+def reviewList():
+    review = list(db.review.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'review': review})
 
 @app.route('/search', methods=["GET"])
 def get_matjip():
     name = request.args.get('name')
-    matjip = list(db.matjip.find({},{'_id':False}))
+    matjip = list(db.matjip.find({}, {'_id': False}))
     search = []
     for i in range(len(matjip)):
         if name in matjip[i]['title']:
             search.append(matjip[i])
-
-    print(search)
-    if (len(search)==0):
+    if (len(search) == 0):
         return jsonify({'result': 'fail'})
     else:
-        return jsonify({'result': 'success', 'matjip':search})
+        return jsonify({'result': 'success', 'matjip': search})
+
+
+@app.route('/review', methods=['POST'])
+def postArticle():
+    title = request.form['title']
+    comment = request.form['comment']
+    pic = request.form['pic']
+    db.review.insert_one({
+        'title': title,
+        'comment': comment,
+        'pic': pic
+    })
+
+    return jsonify({'result': 'success', 'msg': '저장되었습니다!'})
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
